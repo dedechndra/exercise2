2053eda263fdf74e421f3e857584199bb55d16b1
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-10-31 02:11:38 -0700

    Add project files.

---
+++ exercise2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercise2
+{
+    class Program
+    {
+        // Deklarasi array int dengan ukuran 06+20-2x15+10+20 = 26
+        private int[] dede = new int[26];
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array 
+        private int n;
+        // Fungsi / Method untuk menerima masukan 
+        
+    }
+}
+
