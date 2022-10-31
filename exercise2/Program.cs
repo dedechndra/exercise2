18fcb99ca1bd8064d153afd0e04ebc75a544e981
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-10-31 02:15:05 -0700

    Deklarasi array int dengan ukuran maksimal = 26

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -9,7 +9,25 @@ namespace exercise2
         // Deklarasi variabel int untuk menyimpan banyaknya data pada array 
         private int n;
         // Fungsi / Method untuk menerima masukan 
-        
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data yang disimpan pada array 
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 26)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 26 elemen.\n");
+                Console.WriteLine("");
+                Console.WriteLine("------------------------");
+                Console.WriteLine(" Masukkan elemen array ");
+                Console.WriteLine("------------------------");
+            }
+        }
     }
 }
+
 
